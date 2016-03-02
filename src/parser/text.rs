d8de119d27f8af3f15ff2aa01d515663b944ac0f
Alan Jeffrey <ajeffrey@mozilla.com>
2016-03-02 17:40:13 -0600

    Made the lexer do string unescaping.

--- src/parser/text.rs
+++ src/parser/text.rs
@@ -1,13 +1,14 @@
 use self::Token::{Begin, End, Identifier, Number, Text, Whitespace};
-use self::LexError::{UnexpectedChar, UnexpectedEOF, UnclosedString, UnparseableInt};
+use self::LexError::{UnexpectedEscape, UnexpectedChar, UnexpectedEOF, UnclosedString, UnparseableInt};
 use self::ParseError::{LexErr, ExpectedModuleErr, ExpectedEndErr, ExpectedNumberErr, ExpectedTextErr};
 use ast::{Memory, Module, Segment};
 
 use parsell::{Upcast, Downcast, ToStatic, StaticMarker};
 use parsell::{Parser, Uncommitted, Boxable, ParseResult, HasOutput, InState, Stateful};
-use parsell::{character, character_ref, CHARACTER};
+use parsell::{emit, character, character_ref, CHARACTER};
 use std::num::ParseIntError;
 use std::borrow::Cow;
+use std::borrow::Cow::Borrowed;
 use std::str::Chars;
 use std::iter::Peekable;
 use std::vec::Drain;
@@ -20,7 +21,7 @@ pub enum Token<'a> {
     End,
     Identifier(Cow<'a,str>),
     Number(usize),
-    Text(Cow<'a,str>),
+    Text(String),
     Whitespace(Cow<'a,str>),
 }
 
@@ -37,7 +38,7 @@ impl<'a> Downcast<Token<'static>> for Token<'a> {
             End => End,
             Identifier(name) => Identifier(name.downcast()),
             Number(num) => Number(num),
-            Text(string) => Text(string.downcast()),
+            Text(string) => Text(string),
             Whitespace(string) => Whitespace(string.downcast()),
         }
     }
@@ -49,6 +50,7 @@ impl<'a> ToStatic for Token<'a> {
 
 #[derive(Clone, PartialEq, Debug)]
 pub enum LexError {
+    UnexpectedEscape(String),
     UnexpectedChar(char),
     UnclosedString(char),
     UnparseableInt(ParseIntError),
@@ -76,28 +78,32 @@ fn is_keyword_char(ch: char) -> bool { ch.is_alphanumeric() || (ch == '.') }
 fn is_identifier_char(ch: char) -> bool { ch.is_alphanumeric() || (ch == '.') || (ch == '$') }
 fn is_unescaped_char(ch: char) -> bool { ch != '"' && ch != '\\' && ch != '\r' && ch != '\n' }
 
-fn mk_begin<'a>(x: (char, Cow<'a,str>)) -> Result<Token<'a>, LexError> { Ok(Begin(x.1)) }
+fn mk_begin<'a>(s: Cow<'a,str>) -> Result<Token<'a>, LexError> { Ok(Begin(s)) }
 fn mk_end<'a>(_: char) -> Result<Token<'a>, LexError> { Ok(End) }
 fn mk_identifier<'a>(s: Cow<'a,str>) -> Result<Token<'a>, LexError> { Ok(Identifier(s)) }
 fn mk_whitespace<'a>(s: Cow<'a,str>) -> Result<Token<'a>, LexError> { Ok(Whitespace(s)) }
+
+fn mk_escape<'a>(escaped: Cow<'a, str>) -> Result<Cow<'a, str>, LexError> {
+    match &*escaped {
+        "\\\\" => Ok(Borrowed("\\")),
+        "\\\"" => Ok(Borrowed("\"")),
+        "\\n" => Ok(Borrowed("\n")),
+        "\\r" => Ok(Borrowed("\r")),
+        "\\t" => Ok(Borrowed("\t")),
+        _ => Err(UnexpectedEscape(escaped.into_owned())),
+    }
+}
+
+fn mk_unescape<'a>(unescaped: Cow<'a, str>) -> Result<Cow<'a, str>, LexError> {
+    Ok(unescaped)
+}
 
 fn mk_number<'a>(s: Cow<'a,str>) -> Result<Token<'a>, LexError> {
     Ok(Number(try!(usize::from_str_radix(&*s, 10))))
 }
 
-fn mk_text<'a>(s: Cow<'a,str>) -> Result<Token<'a>, LexError> {
-    {
-        let mut chs = s.chars();
-        match chs.next() {
-            Some('"') => match chs.next_back() {
-                Some('"') => (),
-                Some(ch) if !is_unescaped_char(ch) => return Err(UnclosedString(ch)),
-                _ => return Err(UnexpectedEOF),
-            },
-            _ => panic!("mk_text(s) where s doesn't start with '\"'."),
-        }
-    }
-    Ok(Text(s))
+fn mk_text<'a>(s: String) -> Token<'a> {
+    Text(s)
 }
 
 fn mk_unexpected_char_err<'a>(ch: Option<char>) -> Result<Token<'a>,LexError> { Err(ch.map_or(UnexpectedEOF, UnexpectedChar)) }
@@ -107,6 +113,19 @@ fn mk_lexer_state<Lexer>(lexer: Lexer) -> WasmLexerState
 {
     WasmLexer.in_state(Box::new(lexer))
 }
+
+fn mk_ok_string() -> Result<String, LexError> {
+    Ok(String::new())
+}
+
+fn must_be_dbl_quote(ch: Option<char>) -> Result<(), LexError> {
+    if ch == Some('"') {
+        Ok(())
+    } else {
+        Err(ch.map_or(UnexpectedEOF, UnclosedString))
+    }
+}
+
 
 // Work-around for not having impl results yet.
 
@@ -130,7 +149,7 @@ impl<'a> Uncommitted<char, Chars<'a>, Result<Token<'a>, LexError>> for WasmLexer
     fn init(&self, data: &mut Chars<'a>) -> Option<ParseResult<WasmLexerState, Result<Token<'a>, LexError>>> {
 
         let BEGIN = character(is_lparen)
-            .and_then(character(is_keyword_char).star(ignore).buffer())
+            .discard_and_then(character(is_keyword_char).star(ignore).buffer())
             .map(mk_begin);
 
         let END = character(is_rparen)
@@ -148,23 +167,24 @@ impl<'a> Uncommitted<char, Chars<'a>, Result<Token<'a>, LexError>> for WasmLexer
 
         let ESCAPED = character(is_backslash)
             .and_then(CHARACTER)
-            .map2(discard_char2);
+            .buffer()
+            .map(mk_escape);
 
-        let UNESCAPED = character(is_unescaped_char)
-            .map(discard_char1);
-
-        let UNRECOGNIZED = CHARACTER
-            .map(mk_unexpected_char_err);
+        let UNESCAPED = character(is_unescaped_char).plus(ignore)
+            .buffer()
+            .map(mk_unescape);
 
         let TEXT = OPEN_QUOTE
-            .and_then(ESCAPED.or_else(UNESCAPED).star(ignore))
-            .and_then(CHARACTER)
-            .buffer()
-            .map(mk_text);
+            .discard_and_then(ESCAPED.or_else(UNESCAPED).star(mk_ok_string)) // TODO: buffer into a Cow<'a,str>
+            .try_and_then_try_discard(CHARACTER.map(must_be_dbl_quote))
+            .try_map(mk_text);
 
         let NUMBER = character(char::is_numeric).plus(ignore).buffer()
             .map(mk_number);
         
+        let UNRECOGNIZED = CHARACTER
+            .map(mk_unexpected_char_err);
+
         let WASM_TOKEN = IDENTIFIER
             .or_else(BEGIN)
             .or_else(END)
@@ -192,7 +212,7 @@ fn test_lexer() {
     assert_eq!(LEXER.init_str(")!"),Some(Done(Ok(End))));
     assert_eq!(LEXER.init_str("$abc!"),Some(Done(Ok(Identifier(Borrowed("$abc"))))));
     assert_eq!(LEXER.init_str(" \t\r\n !"),Some(Done(Ok(Whitespace(Borrowed(" \t\r\n "))))));
-    assert_eq!(LEXER.init_str("\"xyz\\t\\\"abc\"!"),Some(Done(Ok(Text(Borrowed("\"xyz\\t\\\"abc\""))))));
+    assert_eq!(LEXER.init_str("\"xyz\\t\\\"abc\"!"),Some(Done(Ok(Text(String::from("xyz\t\"abc"))))));
     assert_eq!(LEXER.init_str(" \t\r\n !"),Some(Done(Ok(Whitespace(Borrowed(" \t\r\n "))))));
     assert_eq!(LEXER.init_str("!!"),Some(Done(Err(UnexpectedChar('!')))));
     assert_eq!(LEXER.init_str("\"abc\r\"!"),Some(Done(Err(UnclosedString('\r')))));
@@ -295,151 +315,3 @@ impl<'a> Uncommitted<Token<'a>, Tokens<'a>, Result<Module, ParseError>> for Wasm
 
 }
 
-// impl StaticMarker for ParseError {}
-
-// pub type Tokens<'a> = Peekable<Drain<'a, Token<'a>>>; // A placeholder
-
-// fn is_begin_module<'a>(tok: &Token<'a>) -> bool {
-//     match *tok {
-//         Begin(ref kw) => (kw == "module"),
-//         _ => false,
-//     }
-// }
-
-// fn is_begin_memory<'a>(tok: &Token<'a>) -> bool {
-//     match *tok {
-//         Begin(ref kw) => (kw == "memory"),
-//         _ => false,
-//     }
-// }
-
-// fn is_begin_segment<'a>(tok: &Token<'a>) -> bool {
-//     match *tok {
-//         Begin(ref kw) => (kw == "segment"),
-//         _ => false,
-//     }
-// }
-
-// fn is_number<'a>(tok: &Token<'a>) -> bool {
-//     match *tok {
-//         Number(_) => true,
-//         _ => false,
-//     }
-// }
-
-// fn unwrap_number<'a>(tok: Token<'a>) -> usize {
-//     match tok {
-//         Number(num) => num,
-//         _ => panic!("not a number"),
-//     }
-// }
-
-// fn must_be_number<'a>(tok: Option<Token<'a>>) -> Result<usize, ParseError> {
-//     match tok {
-//         Some(Number(num)) => Ok(num),
-//         _ => Err(ExpectedNumberErr),
-//     }
-// }
-
-// fn must_be_text<'a>(tok: Option<Token<'a>>) -> Result<String, ParseError> {
-//     match tok {
-//         Some(Text(text)) => Ok(text.into_owned()),
-//         _ => Err(ExpectedTextErr),
-//     }
-// }
-
-// fn must_be_end<'a>(tok: Option<Token<'a>>) -> Result<(), ParseError> {
-//     match tok {
-//         Some(End) => Ok(()),
-//         _ => Err(ExpectedEndErr),
-//     }
-// }
-
-// fn mk_segment(addr: usize, data: String) -> Segment { Segment { addr: addr, data: data } }
-// fn mk_memory(init: usize, max: Option<usize>, segments: Vec<Segment>) -> Memory { Memory { init: init, max: max, segments: segments } }
-// fn mk_module() -> Result<Module, ParseError> { Ok(Module::new()) }
-
-// fn mk_vec<T>() -> Result<Vec<T>, ParseError> { Ok(Vec::new()) }
-
-// pub type ParserOutput<T> = Result<T, ParseError>;
-// pub type ParserState<T> = Box<for<'a> Boxable<Token<'a>, Tokens<'a>, Output=Result<T, ParseError>>>;
-
-// fn mk_parser_box<P, T>(parser: P) -> ParserState<T>
-//     where P: 'static + for<'a> Boxable<Token<'a>, Tokens<'a>, Output=Result<T, ParseError>>
-// {
-//     Box::new(parser)
-// }
-
-// #[derive(Copy, Clone, PartialEq, Eq, Hash, Ord, PartialOrd, Debug)]
-// pub struct SegmentParser;
-// impl Parser for SegmentParser {}
-// impl<'a> Uncommitted<Token<'a>, Tokens<'a>> for SegmentParser {
-
-//     type Output = ParserOutput<Segment>;
-//     type State = ParserState<Segment>;
-
-//     #[allow(non_snake_case)]
-//     fn init(&self, data: &mut Tokens<'a>) -> Option<ParseResult<Self::State, Self::Output>> {
-
-//         character_ref(is_begin_segment)
-//             .discard_and_then(CHARACTER.map(must_be_number))
-//             .try_and_then_try(CHARACTER.map(must_be_text))
-//             .try_and_then_try_discard(CHARACTER.map(must_be_end))
-//             .try_map2(mk_segment)
-//             .boxed(mk_parser_box)
-//             .init(data)
-            
-//     }
-
-// }
-
-// pub const SEGMENT: SegmentParser = SegmentParser;
-
-// #[derive(Copy, Clone, PartialEq, Eq, Hash, Ord, PartialOrd, Debug)]
-// pub struct MemoryParser;
-// impl Parser for MemoryParser {}
-// impl<'a> Uncommitted<Token<'a>, Tokens<'a>> for MemoryParser {
-
-//     type Output = ParserOutput<Memory>;
-//     type State = ParserState<Memory>;
-
-//     #[allow(non_snake_case)]
-//     fn init(&self, data: &mut Tokens<'a>) -> Option<ParseResult<Self::State, Self::Output>> {
-
-//         character_ref(is_begin_memory)
-//             .discard_and_then(CHARACTER.map(must_be_number))
-//             .try_and_then(character_ref(is_number).map(unwrap_number).opt())
-//             .try_and_then_try(SEGMENT.star(mk_vec))
-//             .try_and_then_try_discard(CHARACTER.map(must_be_end))
-//             .try_map3(mk_memory)
-//             .boxed(mk_parser_box)
-//             .init(data)
-            
-//     }
-
-// }
-
-// pub const MEMORY: MemoryParser = MemoryParser;
-
-// #[derive(Copy, Clone, PartialEq, Eq, Hash, Ord, PartialOrd, Debug)]
-// pub struct ModuleParser;
-// impl Parser for ModuleParser {}
-// impl<'a> Uncommitted<Token<'a>, Tokens<'a>> for ModuleParser {
-
-//     type Output = ParserOutput<Module>;
-//     type State = ParserState<Module>;
-
-//     #[allow(non_snake_case)]
-//     fn init(&self, data: &mut Tokens<'a>) -> Option<ParseResult<Self::State, Self::Output>> {
-
-//         character_ref(is_begin_module)
-//             .discard_and_then(MEMORY.star(mk_module))
-//             .try_and_then_try_discard(CHARACTER.map(must_be_end))
-//             .boxed(mk_parser_box)
-//             .init(data)
-            
-//     }
-
-// }
-
-// pub const MODULE: ModuleParser = ModuleParser;
