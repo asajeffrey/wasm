1a444f71f290de4e3c3d440fbf30df56178ab43c
Alan Jeffrey <ajeffrey@mozilla.com>
2016-03-29 14:35:46 -0500

    Finished type checking for expressions.

--- wasm-typecheck/src/lib.rs
+++ wasm-typecheck/src/lib.rs
@@ -1,13 +1,21 @@
 extern crate wasm_ast;
 
-use self::TypeError::{BinOpResultErr, ConstTypeErr, GrowMemoryResultErr, NoSuchFunctionErr, FunctionArgCountErr, FunctionResultErr, IfThenResultErr, LoadTypeErr, NopResultErr, StoreTypeErr, UnaryOpResultErr, VarTypeErr};
+use self::TypeError::{BinOpArgsErr, BinOpResultErr, ConstTypeErr, GrowMemoryResultErr, NoSuchFunctionErr,
+                      FunctionArgCountErr, FunctionResultErr, IfThenResultErr, LoadTypeErr, NopResultErr,
+                      NoSuchVarErr, StoreTypeErr, UnaryOpResultErr, UnaryOpArgErr, VarTypeErr};
 use wasm_ast::{BinOp, Const, Export, Expr, Function, Module, SignedTyp, Size, Typ, UnaryOp, VarDec, VarUse};
-use wasm_ast::Expr::{BinOpExpr, CallExpr, ConstExpr, GetLocalExpr, GrowMemoryExpr, IfThenExpr, IfThenElseExpr, LoadExpr, NopExpr, SetLocalExpr, StoreExpr, UnaryOpExpr};
+use wasm_ast::BinOp::{Add, And, Copysign, Div, Eq, Ge, Gt, Le, Lt, Max, Min, Mul, Ne,
+                      Or, Rem, RotL, RotR, Shl, Shr, Sub, Xor};
+use wasm_ast::Const::{F32Const, F64Const, I32Const, I64Const};
+use wasm_ast::Expr::{BinOpExpr, CallExpr, ConstExpr, GetLocalExpr, GrowMemoryExpr, IfThenExpr, IfThenElseExpr,
+                     LoadExpr, NopExpr, SetLocalExpr, StoreExpr, UnaryOpExpr};
 use wasm_ast::SignedTyp::{F32s, F64s, I32s, I64s, U32s, U64s};
 use wasm_ast::Size::{Bits32, Bits64};
 use wasm_ast::Typ::{F32, F64, I32, I64};
+use wasm_ast::UnaryOp::{Abs, Ceil, Clz, Ctz, Eqz, Floor, Nearest, Neg, Popcnt, Sqrt, Trunc};
 
 pub enum TypeError {
+    BinOpArgsErr(SignedTyp, BinOp),
     BinOpResultErr(SignedTyp, BinOp, Option<Typ>),
     ConstTypeErr(Const, Option<Typ>),
     FunctionArgCountErr(String, usize),
@@ -16,8 +24,10 @@ pub enum TypeError {
     IfThenResultErr(Typ),
     LoadTypeErr(SignedTyp, Size, Option<Typ>),
     NoSuchFunctionErr(String),
+    NoSuchVarErr(VarUse),
     NopResultErr(Typ),
     StoreTypeErr(Typ, Size, Option<Typ>),
+    UnaryOpArgErr(Typ, UnaryOp),
     UnaryOpResultErr(Typ, UnaryOp, Option<Typ>),
     VarTypeErr(VarUse, Option<Typ>),
 }
@@ -69,24 +79,124 @@ impl TypeCheck for Module {
 
     fn binary_op_type(&self, src: &SignedTyp, op: &BinOp) -> Result<Typ, TypeError> {
         match (src, op) {
-            _ => panic!("TODO"),
+
+            (&F32s, &Add) => Ok(F32),
+            (&F32s, &Copysign) => Ok(F32),
+            (&F32s, &Div) => Ok(F32),
+            (&F32s, &Max) => Ok(F32),
+            (&F32s, &Min) => Ok(F32),
+            (&F32s, &Mul) => Ok(F32),
+            (&F32s, &Sub) => Ok(F32),
+
+            (&F64s, &Add) => Ok(F64),
+            (&F64s, &Copysign) => Ok(F64),
+            (&F64s, &Div) => Ok(F64),
+            (&F64s, &Max) => Ok(F64),
+            (&F64s, &Min) => Ok(F64),
+            (&F64s, &Mul) => Ok(F64),
+            (&F64s, &Sub) => Ok(F64),
+
+            (&I32s, &Add) => Ok(I32),
+            (&I32s, &Div) => Ok(I32),
+            (&I32s, &Mul) => Ok(I32),
+            (&I32s, &Rem) => Ok(I32),
+
+            (&I64s, &Add) => Ok(I64),
+            (&I64s, &Div) => Ok(I64),
+            (&I64s, &Mul) => Ok(I64),
+            (&I64s, &Rem) => Ok(I64),
+
+            (&U32s, &Add) => Ok(I32),
+            (&U32s, &And) => Ok(I32),
+            (&U32s, &Div) => Ok(I32),
+            (&U32s, &Mul) => Ok(I32),
+            (&U32s, &Or) => Ok(I32),
+            (&U32s, &Rem) => Ok(I32),
+            (&U32s, &RotL) => Ok(I32),
+            (&U32s, &RotR) => Ok(I32),
+            (&U32s, &Shl) => Ok(I32),
+            (&U32s, &Shr) => Ok(I32),
+            (&U32s, &Sub) => Ok(I32),
+            (&U32s, &Xor) => Ok(I32),
+
+            (&U64s, &Add) => Ok(I64),
+            (&U64s, &And) => Ok(I64),
+            (&U64s, &Div) => Ok(I64),
+            (&U64s, &Mul) => Ok(I64),
+            (&U64s, &Or) => Ok(I64),
+            (&U64s, &Rem) => Ok(I64),
+            (&U64s, &RotL) => Ok(I64),
+            (&U64s, &RotR) => Ok(I64),
+            (&U64s, &Shl) => Ok(I64),
+            (&U64s, &Shr) => Ok(I64),
+            (&U64s, &Sub) => Ok(I64),
+            (&U64s, &Xor) => Ok(I64),
+
+            (_, &Eq) => Ok(I32),
+            (_, &Ge) => Ok(I32),
+            (_, &Gt) => Ok(I32),
+            (_, &Le) => Ok(I32),
+            (_, &Lt) => Ok(I32),
+            (_, &Ne) => Ok(I32),
+
+            _ => Err(BinOpArgsErr(*src, *op)),
+
         }
     }
 
     fn unary_op_type(&self, src: &Typ, op: &UnaryOp) -> Result<Typ, TypeError> {
         match (src, op) {
-            _ => panic!("TODO"),
+
+            (&F32, &Abs) => Ok(F32),
+            (&F32, &Ceil) => Ok(F32),
+            (&F32, &Floor) => Ok(F32),
+            (&F32, &Nearest) => Ok(F32),
+            (&F32, &Neg) => Ok(F32),
+            (&F32, &Sqrt) => Ok(F32),
+            (&F32, &Trunc) => Ok(F32),
+
+            (&F64, &Abs) => Ok(F64),
+            (&F64, &Ceil) => Ok(F64),
+            (&F64, &Floor) => Ok(F64),
+            (&F64, &Nearest) => Ok(F64),
+            (&F64, &Neg) => Ok(F64),
+            (&F64, &Sqrt) => Ok(F64),
+            (&F64, &Trunc) => Ok(F64),
+
+            (&I32, &Clz) => Ok(I32),
+            (&I32, &Ctz) => Ok(I32),
+            (&I32, &Popcnt) => Ok(I32),
+            (&I32, &Eqz) => Ok(I32),
+
+            (&I64, &Clz) => Ok(I64),
+            (&I64, &Ctz) => Ok(I64),
+            (&I64, &Popcnt) => Ok(I64),
+
+            _ => Err(UnaryOpArgErr(*src, *op)),
+
         }
     }
 
     fn const_type(&self, constant: &Const) -> Typ {
         match constant {
-            _ => panic!("TODO"),
+            &F32Const(_) => F32,
+            &F64Const(_) => F64,
+            &I32Const(_) => I32,
+            &I64Const(_) => I64,
         }
     }
 
-    fn var_type(&self, vars: &[&VarDec], expr: &VarUse) -> Result<Typ, TypeError> {
-        panic!("TODO")
+    fn var_type(&self, vars: &[&VarDec], var: &VarUse) -> Result<Typ, TypeError> {
+        match vars.get(var.position) {
+            None => Err(NoSuchVarErr(var.clone())),
+            Some(dec) => {
+                match (&var.name, &dec.name) {
+                    (&Some(ref name1), &Some(ref name2)) if name1 != name2 => Err(NoSuchVarErr(var.clone())),
+                    (&Some(_), &None) => Err(NoSuchVarErr(var.clone())),
+                    _ => Ok(dec.typ)
+                }
+            }
+        }
     }
 
     fn type_check_expr(&self, vars: &[&VarDec], expr: &Expr, tgt: &Option<Typ>) -> Result<(), TypeError> {
