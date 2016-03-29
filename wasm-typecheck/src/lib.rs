6f33e3efb84e0658649c9863e8f85fbaaec57e32
Alan Jeffrey <ajeffrey@mozilla.com>
2016-03-29 13:29:05 -0500

    Added type checking for expressions.

--- wasm-typecheck/src/lib.rs
+++ wasm-typecheck/src/lib.rs
@@ -1,21 +1,38 @@
 extern crate wasm_ast;
 
-use self::TypeError::{DiscardedOpErr, NoSuchFunctionErr};
-use wasm_ast::{BinOp, Export, Expr, Function, Import, Module, SignedTyp, Typ, VarDec};
+use self::TypeError::{BinOpResultErr, ConstTypeErr, GrowMemoryResultErr, NoSuchFunctionErr, FunctionArgCountErr, FunctionResultErr, IfThenResultErr, LoadTypeErr, NopResultErr, StoreTypeErr, UnaryOpResultErr, VarTypeErr};
+use wasm_ast::{BinOp, Const, Export, Expr, Function, Module, SignedTyp, Size, Typ, UnaryOp, VarDec, VarUse};
 use wasm_ast::Expr::{BinOpExpr, CallExpr, ConstExpr, GetLocalExpr, GrowMemoryExpr, IfThenExpr, IfThenElseExpr, LoadExpr, NopExpr, SetLocalExpr, StoreExpr, UnaryOpExpr};
 use wasm_ast::SignedTyp::{F32s, F64s, I32s, I64s, U32s, U64s};
+use wasm_ast::Size::{Bits32, Bits64};
 use wasm_ast::Typ::{F32, F64, I32, I64};
 
 pub enum TypeError {
+    BinOpResultErr(SignedTyp, BinOp, Option<Typ>),
+    ConstTypeErr(Const, Option<Typ>),
+    FunctionArgCountErr(String, usize),
+    FunctionResultErr(String, Option<Typ>),
+    GrowMemoryResultErr(Typ),
+    IfThenResultErr(Typ),
+    LoadTypeErr(SignedTyp, Size, Option<Typ>),
     NoSuchFunctionErr(String),
-    DiscardedOpErr,
-    OpTypeError,
+    NopResultErr(Typ),
+    StoreTypeErr(Typ, Size, Option<Typ>),
+    UnaryOpResultErr(Typ, UnaryOp, Option<Typ>),
+    VarTypeErr(VarUse, Option<Typ>),
 }
 
 pub trait TypeCheck {
+    fn size_of_type(&self, typ: &Typ) -> Size;
     fn unsigned_type(&self, typ: &SignedTyp) -> Typ;
+
     fn lookup_function(&self, name: &str) -> Result<&Function, TypeError>;
-    fn type_check_binop(&self, src: &SignedTyp, binop: &BinOp, tgt: &Typ) -> Result<(), TypeError>;
+
+    fn binary_op_type(&self, src: &SignedTyp, op: &BinOp) -> Result<Typ, TypeError>;
+    fn unary_op_type(&self, src: &Typ, op: &UnaryOp) -> Result<Typ, TypeError>;
+    fn const_type(&self, constant: &Const) -> Typ;
+    fn var_type(&self, vars: &[&VarDec], expr: &VarUse) -> Result<Typ, TypeError>;
+
     fn type_check_expr(&self, vars: &[&VarDec], expr: &Expr, typ: &Option<Typ>) -> Result<(), TypeError>;
     fn type_check_function(&self, function: &Function) -> Result<(), TypeError>;
     fn type_check_export(&self, export: &Export) -> Result<(), TypeError>;
@@ -30,6 +47,15 @@ impl TypeCheck for Module {
             .map_or_else (|| Err(NoSuchFunctionErr(String::from(name))), Ok)
     }
 
+    fn size_of_type(&self, typ: &Typ) -> Size {
+        match typ {
+            &F32 => Bits32,
+            &F64 => Bits64,
+            &I32 => Bits32,
+            &I64 => Bits64,
+        }
+    }
+
     fn unsigned_type(&self, typ: &SignedTyp) -> Typ {
         match typ {
             &F32s => F32,
@@ -41,22 +67,109 @@ impl TypeCheck for Module {
         }
     }
 
-    fn type_check_binop(&self, src: &SignedTyp, binop: &BinOp, tgt: &Typ) -> Result<(), TypeError> {
-        match (src, binop, tgt) {
+    fn binary_op_type(&self, src: &SignedTyp, op: &BinOp) -> Result<Typ, TypeError> {
+        match (src, op) {
             _ => panic!("TODO"),
         }
     }
 
+    fn unary_op_type(&self, src: &Typ, op: &UnaryOp) -> Result<Typ, TypeError> {
+        match (src, op) {
+            _ => panic!("TODO"),
+        }
+    }
+
+    fn const_type(&self, constant: &Const) -> Typ {
+        match constant {
+            _ => panic!("TODO"),
+        }
+    }
+
+    fn var_type(&self, vars: &[&VarDec], expr: &VarUse) -> Result<Typ, TypeError> {
+        panic!("TODO")
+    }
+
     fn type_check_expr(&self, vars: &[&VarDec], expr: &Expr, tgt: &Option<Typ>) -> Result<(), TypeError> {
         match expr {
-            &BinOpExpr(ref src, ref binop, ref lhs, ref rhs) => {
-                let tgt = try!(tgt.map_or(Err(DiscardedOpErr), Ok));
+            &BinOpExpr(ref src, ref op, ref lhs, ref rhs) => {
                 let usrc = self.unsigned_type(src);
-                try!(self.type_check_binop(src, binop, &tgt));
                 try!(self.type_check_expr(vars, lhs, &Some(usrc)));
                 try!(self.type_check_expr(vars, rhs, &Some(usrc)));
+                if tgt != &Some(try!(self.binary_op_type(src, op))) {
+                    return Err(BinOpResultErr(*src, *op, *tgt));
+                }
             },
-            _ => panic!("TODO"),
+            &CallExpr(ref name, ref args) => {
+                let function = try!(self.lookup_function(name));
+                if function.params.len() != args.len() {
+                    return Err(FunctionArgCountErr(name.clone(), args.len()));
+                }
+                for (arg, param) in args.iter().zip(function.params.iter()) {
+                    try!(self.type_check_expr(vars, arg, &Some(param.typ)));
+                }
+                if &function.result != tgt {
+                    return Err(FunctionResultErr(name.clone(), *tgt))
+                }
+            },
+            &ConstExpr(ref constant) => {
+                if tgt != &Some(self.const_type(constant)) {
+                    return Err(ConstTypeErr(*constant, *tgt));
+                }
+            },
+            &GetLocalExpr(ref var) => {
+                if tgt != &Some(try!(self.var_type(vars, var))) {
+                    return Err(VarTypeErr(var.clone(), *tgt));
+                }
+            },
+            &GrowMemoryExpr(ref size) => {
+                try!(self.type_check_expr(vars, size, &Some(I32)));
+                if let &Some(ref tgt) = tgt {
+                    return Err(GrowMemoryResultErr(*tgt))
+                }
+            },
+            &IfThenExpr(ref cond, ref true_branch) => {
+                try!(self.type_check_expr(vars, cond, &Some(I32)));
+                try!(self.type_check_expr(vars, true_branch, tgt));
+                if let &Some(ref tgt) = tgt {
+                    return Err(IfThenResultErr(*tgt))
+                }
+            },
+            &IfThenElseExpr(ref cond, ref true_branch, ref false_branch) => {
+                try!(self.type_check_expr(vars, cond, &Some(I32)));
+                try!(self.type_check_expr(vars, true_branch, tgt));
+                try!(self.type_check_expr(vars, false_branch, tgt));
+            },
+            &LoadExpr(ref src, ref size, ref addr) => {
+                let usrc = self.unsigned_type(src);
+                try!(self.type_check_expr(vars, addr, &Some(I32)));
+                if tgt != &Some(usrc) || *size > self.size_of_type(&usrc) {
+                    return Err(LoadTypeErr(*src, *size, *tgt))
+                }
+            },
+            &NopExpr => {
+                if let &Some(ref tgt) = tgt {
+                    return Err(NopResultErr(*tgt));
+                }
+            },
+            &SetLocalExpr(ref var, ref rhs) => {
+                try!(self.type_check_expr(vars, rhs, tgt));
+                if tgt != &Some(try!(self.var_type(vars, var))) {
+                    return Err(VarTypeErr(var.clone(), *tgt));
+                }
+            },
+            &StoreExpr(ref src, ref size, ref addr, ref rhs) => {
+                try!(self.type_check_expr(vars, addr, &Some(I32)));
+                try!(self.type_check_expr(vars, rhs, tgt));
+                if tgt != &Some(*src) || *size > self.size_of_type(src) {
+                    return Err(StoreTypeErr(*src, *size, *tgt))
+                }
+            },
+            &UnaryOpExpr(ref src, ref op, ref arg) => {
+                try!(self.type_check_expr(vars, arg, &Some(*src)));
+                if tgt != &Some(try!(self.unary_op_type(src, op))) {
+                    return Err(UnaryOpResultErr(*src, *op, *tgt));
+                }
+            },
         }
         Ok(())
     }
